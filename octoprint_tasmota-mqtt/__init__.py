30a622a1e917db8bfc9d759fa8c656b0dec3296f
jneilliii <jneilliii+github@gmail.com>
2017-12-02 00:11:17 -0500

    0.1.0

---
+++ octoprint_tasmota-mqtt/__init__.py
@@ -0,0 +1,102 @@
+# coding=utf-8
+from __future__ import absolute_import
+
+### (Don't forget to remove me)
+# This is a basic skeleton for your plugin's __init__.py. You probably want to adjust the class name of your plugin
+# as well as the plugin mixins it's subclassing from. This is really just a basic skeleton to get you started,
+# defining your plugin as a template plugin, settings and asset plugin. Feel free to add or remove mixins
+# as necessary.
+#
+# Take a look at the documentation on what other plugin mixins are available.
+
+import octoprint.plugin
+
+class Tasmota-mqttPlugin(octoprint.plugin.SettingsPlugin,
+                         octoprint.plugin.AssetPlugin,
+                         octoprint.plugin.TemplatePlugin,
+						 octoprint.plugin.StartupPlugin):
+
+	##~~ SettingsPlugin mixin
+
+	def get_settings_defaults(self):
+		return dict(
+			topic = "sonoff",
+			currentstate = "UNKNOWN"
+		)
+		
+	##~~ StartupPlugin mixin
+	
+	def on_after_startup(self):
+		helpers = self._plugin_manager.get_helpers("mqtt", "mqtt_publish", "mqtt_subscribe", "mqtt_unsubscribe")
+		if helpers:
+			if "mqtt_publish" in helpers:
+				self.mqtt_publish = helpers["mqtt_publish"]
+			if "mqtt_subscribe" in helpers:
+				self.mqtt_subscribe = helpers["mqtt_subscribe"]
+			if "mqtt_unsubscribe" in helpers:
+				self.mqtt_unsubscribe = helpers["mqtt_unsubscribe"]
+
+		self.mqtt_publish("octoprint/plugin/tasmota/pub", "OctoPrint-TasmotaMQTT publishing.")
+		self.mqtt_subscribe("%s/stat/POWER" % self.settings.get(), self._on_mqtt_subscription)
+
+	def _on_mqtt_subscription(self, topic, message, retained=None, qos=None, *args, **kwargs):
+		self._logger.info("Received message for {topic}: {message}".format(**locals()))
+		self.mqtt_publish("octoprint/plugin/tasmota/pub", "echo: " + message)
+		self._settings.set(["currentstate"], message)
+		#self._settings.save()
+
+	##~~ AssetPlugin mixin
+
+	def get_assets(self):
+		# Define your plugin's asset files to automatically include in the
+		# core UI here.
+		return dict(
+			js=["js/tasmota-mqtt.js"],
+			css=["css/tasmota-mqtt.css"]
+		)
+		
+	##~~ TemplatePlugin mixin
+	
+	def get_template_configs(self):
+		return [
+			dict(type="navbar", custom_bindings=False),
+			dict(type="settings", custom_bindings=False)
+		]
+	
+	##~~ Softwareupdate hook
+
+	def get_update_information(self):
+		# Define the configuration for your plugin to use with the Software Update
+		# Plugin here. See https://github.com/foosel/OctoPrint/wiki/Plugin:-Software-Update
+		# for details.
+		return dict(
+			tasmota-mqtt=dict(
+				displayName="Tasmota-mqtt Plugin",
+				displayVersion=self._plugin_version,
+
+				# version check: github repository
+				type="github_release",
+				user="jneilliii",
+				repo="OctoPrint-TasmotaMQTT",
+				current=self._plugin_version,
+
+				# update method: pip
+				pip="https://github.com/jneilliii/OctoPrint-TasmotaMQTT/archive/{target_version}.zip"
+			)
+		)
+
+
+# If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
+# ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
+# can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
+__plugin_name__ = "Tasmota-mqtt Plugin"
+
+def __plugin_load__():
+	global __plugin_implementation__
+	__plugin_implementation__ = Tasmota-mqttPlugin()
+
+	global __plugin_hooks__
+	__plugin_hooks__ = {
+		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+	}
+
---
+++ octoprint_tasmota-mqtt/static/js/tasmota-mqtt.js
@@ -0,0 +1,36 @@
+/*
+ * View model for OctoPrint-TasmotaMQTT
+ *
+ * Author: jneilliii
+ * License: AGPLv3
+ */
+$(function() {
+    function Tasmota-mqttViewModel(parameters) {
+        var self = this;
+
+        self.loginStateViewModel = parameters[0];
+        self.settingsViewModel = parameters[1];
+
+        self.topic = ko.observable();
+		self.currentstate = ko.observable();
+		
+		self.onBeforeBinding = function() {		
+			self.topic(self.settingsViewModel.settings.plugins.tasmota-mqtt.topic());
+			self.currentstate(self.settingsViewModel.settings.plugins.tasmota-mqtt.currentstate());
+        }
+
+		
+    }
+
+    /* view model class, parameters for constructor, container to bind to
+     * Please see http://docs.octoprint.org/en/master/plugins/viewmodels.html#registering-custom-viewmodels for more details
+     * and a full list of the available options.
+     */
+    OCTOPRINT_VIEWMODELS.push({
+        construct: Tasmota-mqttViewModel,
+        // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
+        dependencies: ["loginStateViewModel", "settingsViewModel"],
+        // Elements to bind to, e.g. #settings_plugin_tasmota-mqtt, #tab_plugin_tasmota-mqtt, ...
+        elements: ["#settings_plugin_tasmota-mqtt","#navbar_plugin_tasmota-mqtt"]
+    });
+});
