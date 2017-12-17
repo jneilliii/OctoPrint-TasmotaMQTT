1eac190219b5cba3abb39f5212ffb1f8de5318c7
jneilliii <jneilliii+github@gmail.com>
2017-12-17 01:42:27 -0500

    test

--- octoprint_tasmota_mqtt/static/js/tasmota_mqtt.js
+++ octoprint_tasmota_mqtt/static/js/tasmota_mqtt.js
@@ -23,7 +23,7 @@ $(function() {
 				self.processing('');
 				return;
 			}
-			
+			console.log(data);
 			if (data.noMQTT) {
 				new PNotify({
 							title: 'Tasmota-MQTT Error',
@@ -32,7 +32,7 @@ $(function() {
 							hide: false
 							});
 			} else {
-				var relay = ko.utils.arrayFirst(self.settingsViewModel.settings.plugins.tasmota_mqtt.arrRelays(),function(item){
+				var relay = ko.utils.arrayFirst(self.arrRelays(),function(item){
 					return (item.topic() == data.topic) && (item.relayN() == data.relayN);
 					}) || {'topic':data.topic,'relayN':data.relayN,'currentstate':'UNKNOWN'};
 				relay.currentstate(data.currentstate);
