5cbd92d12bc6f19366d4dbf68446c0f65bf4dd45
jneilliii <jneilliii+github@gmail.com>
2017-12-17 01:56:21 -0500

    test

--- octoprint_tasmota_mqtt/static/js/tasmota_mqtt.js
+++ octoprint_tasmota_mqtt/static/js/tasmota_mqtt.js
@@ -32,7 +32,7 @@ $(function() {
 							hide: false
 							});
 			} else {
-				var relay = ko.utils.arrayFirst(self.arrRelays(),function(item){
+				var relay = ko.utils.arrayFirst(self.settingsViewModel.settings.plugins.tasmota_mqtt.arrRelays(),function(item){
 					return (item.topic() == data.topic) && (item.relayN() == data.relayN);
 					}) || {'topic':data.topic,'relayN':data.relayN,'currentstate':'UNKNOWN'};
 				relay.currentstate(data.currentstate);
