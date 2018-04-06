84183239595b1850d47c46a76de5a078f06b95d7
jneilliii <jneilliii+github@gmail.com>
2018-04-05 20:44:49 -0400

    0.2.2
    Fixed gcode processing issue for single relay devices for #4

--- octoprint_tasmota_mqtt/__init__.py
+++ octoprint_tasmota_mqtt/__init__.py
@@ -144,9 +144,12 @@ class TasmotaMQTTPlugin(octoprint.plugin.SettingsPlugin,
 	
 	def processGCODE(self, comm_instance, phase, cmd, cmd_type, gcode, *args, **kwargs):
 		if gcode:
-			if cmd.startswith("M8") and cmd.count(" ") >= 2:
+			if cmd.startswith("M8") and cmd.count(" ") >= 1:
 				topic = cmd.split()[1]
-				relayN = cmd.split()[2]	
+				if cmd.count(" ") == 2:
+					relayN = cmd.split()[2]
+				else:
+					relayN = ""
 				for relay in self._settings.get(["arrRelays"]):
 					if relay["topic"].upper() == topic.upper() and relay["relayN"] == relayN and relay["gcode"]:
 						if cmd.startswith("M80"):
